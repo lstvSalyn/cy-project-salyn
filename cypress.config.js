4b48f070f305b1a54ffb8c4ff6aa353e45bc2017
lstvSalyn <lstsalyn@gmail.com>
2026-02-28 14:00:34 +0800

    Initial Cypress automation setup

---
+++ cypress.config.js
@@ -0,0 +1,19 @@
+require("dotenv").config();
+const { defineConfig } = require("cypress");
+module.exports = defineConfig({
+e2e: {
+baseUrl: process.env.CYPRESS_BASE_URL || "http://localhost:3000",
+specPattern: "cypress/e2e/**/*.cy.js",
+reporter: "cypress-mochawesome-reporter",
+reporterOptions: {
+reportDir: "reports/mocha",
+overwrite: false,
+html: false,
+json: true,
+},
+setupNodeEvents(on, config) {
+require("cypress-mochawesome-reporter/plugin")(on);
+return config;
+},
+},
+});
